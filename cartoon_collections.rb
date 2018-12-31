f5f2fcb0a1e47942ce969022833afd8ae189d13d
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-12-31 06:34:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheddar_cheese) 
-  cheese_types = ["cheddar", "gouda", "camemebert"]
+  cheese_types = %w (cheddar gouda camemebert)
   cheddar_cheese.find {|cheese| cheese_types.include?(cheese)}
 end
 
